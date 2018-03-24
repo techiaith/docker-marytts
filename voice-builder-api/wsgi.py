eafd2e9fbc03195c09a2835b72e331e7b72dff75
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2018-03-24 09:53:13 +0000

    ychwanegu cychwyn weinydd MaryTTS o fewn voice-builder-api

--- voice-builder-api/wsgi.py
+++ voice-builder-api/wsgi.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
-import cherrypy
-import logging
+import os
+import shlex
 
 import subprocess
 import tempfile
-import os
+
+import cherrypy
+import logging
 
 from voice_builder_api_tasks import generate_voice
 
 class GenerateVoice(object):
 
     def __init__(self):
-        pass
- 
-    def error_page_default(status, message, traceback, version):
-        """Handles any server errors, and returns a json response back to the user"""
-        return u"ERROR %s: %s" % (status, message)
+        self.startMaryTTS()
+
+    def startMaryTTS(self):
+
+        marytts_home = os.environ['MARYTTS_HOME']
+        marytts_version = os.environ['MARYTTS_VERSION']
+        marytts_base = os.path.join(marytts_home, 'target', "marytts-" + marytts_version)        
+        cmd = 'java -showversion -Xms40m -Xmx1g -cp "%s/lib/*" -Dmary.base="%s" marytts.server.Mary' % (marytts_base, marytts_base,)
+        popen_cmd = shlex.split(cmd)
+        self.marytts_pid = subprocess.Popen(popen_cmd).pid
+        cherrypy.log("Started MaryTTS server successfully pid:%s" % (self.marytts_pid))
+
 
     @cherrypy.expose
     def generate_voice(self, uid, **kwargs):
