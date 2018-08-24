6898e99b6592a8809aae2063cca48db8e8e5b9c2
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2018-08-24 12:22:52 +0100

    dileu ffeiliau cmu_arctic

--- voice-builder/cmu_arctic/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_slt_arctic-0.95-release.tar.bz2
-bzip2 -d cmu_us_slt_arctic-0.95-release.tar.bz2
-tar xvf cmu_us_slt_arctic-0.95-release.tar
-rm cmu_us_slt_arctic-0.95-release.tar
-ln -s cmu_us_slt_arctic/wav wav
-cp cmu_us_slt_arctic/etc/txt.done.data txt.done.data
